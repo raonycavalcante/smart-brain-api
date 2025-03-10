7098606d63578d131b56cc8d0abe417379f2130a
Raony Cavalcante <raonycavalcante@gmail.com>
2025-03-10 18:59:48 -0500

    'adding env and modfied server.js'

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    port: 5432,
-    user: "postgres",
-    password: "1601",
-    database: "smart-brain",
+    connectionString: process.env.DATABASE_URL, // Use the Render database URL
+    ssl: { rejectUnauthorized: false }, // Required for Render's PostgreSQL
   },
 });
 const clarifai = require("./controller/clarifai");
